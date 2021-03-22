7fe9e4a0f03340e9b7270ef3a26b0a7a8f13adc6
brunodmn <53493535+brunodmn@users.noreply.github.com>
2021-03-22 15:43:45 -0300

    Update multi_sort.dart
    Hi, 
     It's a great job so far, I propose some extra sorting supports by datetime, string, enum and bool. I needed it, so a made, if you thing its interesting could add on package too.
    
    Thanks

--- lib/multi_sort.dart
+++ lib/multi_sort.dart
@@ -1,21 +1,73 @@
 library multi_sort;
 
+enum MultiSortType {
+  /// sort datetime type elements
+  datetime,
+  /// sort number elements (int/double)
+  number,
+  /// sort generic elements (string, bool, enum)
+  generic
+}
+
 extension MultiSort on List {
-  multisort(List<bool> criteria, dynamic preferrence) {
+  multisort(
+      List<bool> criteria, dynamic preferrence, List<MultiSortType> type) {
     if (preferrence.length == 0 || criteria.length == 0 || this.length == 0)
       return this;
-    if (preferrence.length != criteria.length) {
-      print('Criteria length is not equal to preferrence');
-      return this;
-    }
+
+    assert((preferrence.length == criteria.length)  && (criteria.length == type.length),
+        'Criteria, preferrence and type MUST HAVE same size}');
 
     int compare(int i, dynamic a, dynamic b) {
-      if (a.get(preferrence[i]) == b.get(preferrence[i]))
-        return 0;
-      else if (a.get(preferrence[i]) > b.get(preferrence[i]))
-        return criteria[i] ? 1 : -1;
-      else
-        return criteria[i] ? -1 : 1;
+      switch (type[i]) {
+        case MultiSortType.datetime:
+          try {
+            if (a.get(preferrence[i]).isAtSameMomentAs(b.get(preferrence[i])))
+              return 0;
+            else if (a.get(preferrence[i]).isBefore(b.get(preferrence[i])))
+              return criteria[i] ? 1 : -1;
+            else
+              return criteria[i] ? -1 : 1;
+          } catch (e) {
+            print('multi_sort: Error,  $e');
+            return 0;
+          }
+
+          break;
+        case MultiSortType.number:
+          try {
+            if (a.get(preferrence[i]) == b.get(preferrence[i]))
+              return 0;
+            else if (a.get(preferrence[i]) > b.get(preferrence[i]))
+              return criteria[i] ? 1 : -1;
+            else
+              return criteria[i] ? -1 : 1;
+          } catch (e) {
+            print('multi_sort: Error,  $e');
+          }
+
+          break;
+        case MultiSortType.generic:
+          //Works with ascii, bool, enum, etc (convert to string before sort)
+          try {
+            return criteria[i]
+                ? a
+                    .get(preferrence[i])
+                    .toString()
+                    .toLowerCase()
+                    .compareTo(b.get(preferrence[i]).toString().toLowerCase())
+                : b
+                    .get(preferrence[i])
+                    .toString()
+                    .toLowerCase()
+                    .compareTo(a.get(preferrence[i]).toString().toLowerCase());
+          } catch (e) {
+            print('multi_sort: Error,  $e');
+            return 0;
+          }
+
+          break;
+      }
     }
 
     int sortall(a, b) {
