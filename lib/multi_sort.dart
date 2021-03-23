77babdd879499e085d37b5803873e79ec516d72c
brunodmn <53493535+brunodmn@users.noreply.github.com>
2021-03-23 10:21:34 -0300

    Update multi_sort.dart
    Update:
    - Typoo correction: preferrence changed to preference ( one 'r' subtracted).
     - Preference type changed: Now its List<String>.
     - Assert changed: User HAS TO use non empty preference, otherwise there's no need for using Multisort (before it returned the list itself).
     Criteria and type parameters are optional, BUT if you use it, has to be same size as preference.

--- lib/multi_sort.dart
+++ lib/multi_sort.dart
@@ -3,77 +3,95 @@ library multi_sort;
 enum MultiSortType {
   /// sort datetime type elements
   datetime,
+
   /// sort number elements (int/double)
   number,
+
   /// sort generic elements (string, bool, enum)
   generic
 }
 
 extension MultiSort on List {
-  multisort(
-      List<bool> criteria, dynamic preferrence, List<MultiSortType> type) {
-    if (preferrence.length == 0 || criteria.length == 0 || this.length == 0)
-      return this;
+  multisort(List<String> preference,
+      {List<bool> criteria, List<MultiSortType> type}) {
+    assert((preference.length > 0),
+        'MultiSort: Preference lenght must NOT me empty.');
+        
+        if(criteria != null){
+           assert((criteria.length == preference.length ),
+        'MultiSort: Criteria list has to be same size as preference list.');
+        }
 
-    assert((preferrence.length == criteria.length)  && (criteria.length == type.length),
-        'Criteria, preferrence and type MUST HAVE same size}');
-
+        if(type != null){
+           assert((type.length == preference.length ),
+        'MultiSort: Type list has to be same size as preference list.');
+        }
+   
     int compare(int i, dynamic a, dynamic b) {
-      switch (type[i]) {
+      MultiSortType _type = type == null ? MultiSortType.generic : type[i];
+      bool _criteria = criteria == null ? true : criteria[i];
+      
+
+      switch (_type) {
         case MultiSortType.datetime:
           try {
-            if (a.get(preferrence[i]).isAtSameMomentAs(b.get(preferrence[i])))
+            if (a.get(preference[i]).isAtSameMomentAs(b.get(preference[i])))
               return 0;
-            else if (a.get(preferrence[i]).isBefore(b.get(preferrence[i])))
-              return criteria[i] ? 1 : -1;
+            else if (a.get(preference[i]).isBefore(b.get(preference[i])))
+              return _criteria ? 1 : -1;
             else
-              return criteria[i] ? -1 : 1;
+              return _criteria ? -1 : 1;
           } catch (e) {
-            print('multi_sort: Error,  $e');
+            print(
+                'MultiSort: Error, $e . \n Try using another[MultiSortType] or open an issue on Github');
             return 0;
           }
 
           break;
         case MultiSortType.number:
           try {
-            if (a.get(preferrence[i]) == b.get(preferrence[i]))
+            if (a.get(preference[i]) == b.get(preference[i]))
               return 0;
-            else if (a.get(preferrence[i]) > b.get(preferrence[i]))
-              return criteria[i] ? 1 : -1;
+            else if (a.get(preference[i]) > b.get(preference[i]))
+              return _criteria ? 1 : -1;
             else
-              return criteria[i] ? -1 : 1;
+              return _criteria ? -1 : 1;
           } catch (e) {
-            print('multi_sort: Error,  $e');
+            print(
+                'MultiSort: Error, $e . \n Try using another[MultiSortType] or open an issue on Github');
+            return 0;
           }
 
           break;
         case MultiSortType.generic:
           //Works with ascii, bool, enum, etc (convert to string before sort)
           try {
-            return criteria[i]
+            return _criteria
                 ? a
-                    .get(preferrence[i])
+                    .get(preference[i])
                     .toString()
                     .toLowerCase()
-                    .compareTo(b.get(preferrence[i]).toString().toLowerCase())
+                    .compareTo(b.get(preference[i]).toString().toLowerCase())
                 : b
-                    .get(preferrence[i])
+                    .get(preference[i])
                     .toString()
                     .toLowerCase()
-                    .compareTo(a.get(preferrence[i]).toString().toLowerCase());
+                    .compareTo(a.get(preference[i]).toString().toLowerCase());
           } catch (e) {
-            print('multi_sort: Error,  $e');
+            print(
+                'MultiSort: Error, $e . \n Try using another[MultiSortType] or open an issue on Github');
             return 0;
           }
-
           break;
       }
+      print('MultiSort: Error, MultiSortType not found.');
+      return 0;
     }
 
     int sortall(a, b) {
       int i = 0;
       int result;
-      while (i < preferrence.length) {
+      while (i < preference.length) {
         result = compare(i, a, b);
         if (result != 0) break;
         i++;
